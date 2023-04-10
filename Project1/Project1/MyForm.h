c77e2181208bd7c78601ddbde8511593ca9e1d6b
qwer112358 <rs1409089988@gmail.com>
2023-04-10 18:46:51 +0500

    Added message box

--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <cmath>
 
 namespace Project1 {
 
@@ -34,29 +35,6 @@ namespace Project1 {
 				delete components;
 			}
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	private: System::Windows::Forms::Label^ label2;
@@ -174,16 +152,16 @@ namespace Project1 {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(26, 30);
+			this->label2->Location = System::Drawing::Point(14, 58);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(12, 13);
+			this->label2->Size = System::Drawing::Size(13, 13);
 			this->label2->TabIndex = 25;
 			this->label2->Text = L"L";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(26, 50);
+			this->label3->Location = System::Drawing::Point(14, 78);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(14, 13);
 			this->label3->TabIndex = 26;
@@ -192,16 +170,16 @@ namespace Project1 {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(26, 90);
+			this->label4->Location = System::Drawing::Point(14, 118);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(22, 13);
+			this->label4->Size = System::Drawing::Size(24, 13);
 			this->label4->TabIndex = 27;
 			this->label4->Text = L"Фл";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(26, 70);
+			this->label5->Location = System::Drawing::Point(14, 98);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(15, 13);
 			this->label5->TabIndex = 28;
@@ -210,7 +188,7 @@ namespace Project1 {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(26, 110);
+			this->label6->Location = System::Drawing::Point(14, 138);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(20, 13);
 			this->label6->TabIndex = 29;
@@ -219,7 +197,7 @@ namespace Project1 {
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(26, 210);
+			this->label7->Location = System::Drawing::Point(14, 238);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(13, 13);
 			this->label7->TabIndex = 30;
@@ -228,77 +206,76 @@ namespace Project1 {
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(26, 130);
+			this->label8->Location = System::Drawing::Point(14, 158);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(19, 13);
+			this->label8->Size = System::Drawing::Size(20, 13);
 			this->label8->TabIndex = 31;
 			this->label8->Text = L"Kз";
 			// 
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Location = System::Drawing::Point(26, 150);
+			this->label9->Location = System::Drawing::Point(14, 178);
 			this->label9->Name = L"label9";
-			this->label9->Size = System::Drawing::Size(13, 13);
+			this->label9->Size = System::Drawing::Size(14, 13);
 			this->label9->TabIndex = 32;
 			this->label9->Text = L"Z";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(50, 27);
+			this->textBox1->Location = System::Drawing::Point(38, 55);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 34;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(50, 47);
+			this->textBox2->Location = System::Drawing::Point(38, 75);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 22);
+			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 35;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(50, 67);
+			this->textBox3->Location = System::Drawing::Point(38, 95);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(100, 22);
+			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 36;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(50, 87);
+			this->textBox4->Location = System::Drawing::Point(38, 115);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(100, 22);
+			this->textBox4->Size = System::Drawing::Size(100, 20);
 			this->textBox4->TabIndex = 37;
-			this->textBox4->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox4_TextChanged);
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(50, 107);
+			this->textBox5->Location = System::Drawing::Point(38, 135);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(100, 22);
+			this->textBox5->Size = System::Drawing::Size(100, 20);
 			this->textBox5->TabIndex = 38;
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(50, 127);
+			this->textBox6->Location = System::Drawing::Point(38, 155);
 			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(100, 22);
+			this->textBox6->Size = System::Drawing::Size(100, 20);
 			this->textBox6->TabIndex = 39;
 			// 
 			// label11
 			// 
 			this->label11->AutoSize = true;
-			this->label11->Location = System::Drawing::Point(382, 76);
+			this->label11->Location = System::Drawing::Point(390, 255);
 			this->label11->Name = L"label11";
-			this->label11->Size = System::Drawing::Size(27, 13);
+			this->label11->Size = System::Drawing::Size(25, 13);
 			this->label11->TabIndex = 43;
 			this->label11->Text = L"[лк]";
 			// 
 			// label15
 			// 
 			this->label15->AutoSize = true;
-			this->label15->Location = System::Drawing::Point(26, 170);
+			this->label15->Location = System::Drawing::Point(14, 198);
 			this->label15->Name = L"label15";
 			this->label15->Size = System::Drawing::Size(23, 13);
 			this->label15->TabIndex = 46;
@@ -307,76 +284,75 @@ namespace Project1 {
 			// label19
 			// 
 			this->label19->AutoSize = true;
-			this->label19->Location = System::Drawing::Point(26, 230);
+			this->label19->Location = System::Drawing::Point(14, 258);
 			this->label19->Name = L"label19";
-			this->label19->Size = System::Drawing::Size(14, 13);
+			this->label19->Size = System::Drawing::Size(13, 13);
 			this->label19->TabIndex = 50;
 			this->label19->Text = L"η";
-			this->label19->Click += gcnew System::EventHandler(this, &MyForm::label19_Click);
 			// 
 			// label20
 			// 
 			this->label20->AutoSize = true;
-			this->label20->Location = System::Drawing::Point(26, 250);
+			this->label20->Location = System::Drawing::Point(14, 278);
 			this->label20->Name = L"label20";
-			this->label20->Size = System::Drawing::Size(14, 13);
+			this->label20->Size = System::Drawing::Size(13, 13);
 			this->label20->TabIndex = 51;
 			this->label20->Text = L"μ";
 			// 
 			// label23
 			// 
 			this->label23->AutoSize = true;
-			this->label23->Location = System::Drawing::Point(26, 190);
+			this->label23->Location = System::Drawing::Point(14, 218);
 			this->label23->Name = L"label23";
-			this->label23->Size = System::Drawing::Size(19, 13);
+			this->label23->Size = System::Drawing::Size(20, 13);
 			this->label23->TabIndex = 54;
 			this->label23->Text = L"Sn";
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(50, 147);
+			this->textBox7->Location = System::Drawing::Point(38, 175);
 			this->textBox7->Name = L"textBox7";
-			this->textBox7->Size = System::Drawing::Size(100, 22);
+			this->textBox7->Size = System::Drawing::Size(100, 20);
 			this->textBox7->TabIndex = 55;
 			// 
 			// textBox8
 			// 
-			this->textBox8->Location = System::Drawing::Point(50, 167);
+			this->textBox8->Location = System::Drawing::Point(38, 195);
 			this->textBox8->Name = L"textBox8";
-			this->textBox8->Size = System::Drawing::Size(100, 22);
+			this->textBox8->Size = System::Drawing::Size(100, 20);
 			this->textBox8->TabIndex = 56;
 			// 
 			// textBox9
 			// 
-			this->textBox9->Location = System::Drawing::Point(50, 187);
+			this->textBox9->Location = System::Drawing::Point(38, 215);
 			this->textBox9->Name = L"textBox9";
-			this->textBox9->Size = System::Drawing::Size(100, 22);
+			this->textBox9->Size = System::Drawing::Size(100, 20);
 			this->textBox9->TabIndex = 57;
 			// 
 			// textBox10
 			// 
-			this->textBox10->Location = System::Drawing::Point(50, 207);
+			this->textBox10->Location = System::Drawing::Point(38, 235);
 			this->textBox10->Name = L"textBox10";
-			this->textBox10->Size = System::Drawing::Size(100, 22);
+			this->textBox10->Size = System::Drawing::Size(100, 20);
 			this->textBox10->TabIndex = 58;
 			// 
 			// textBox11
 			// 
-			this->textBox11->Location = System::Drawing::Point(50, 227);
+			this->textBox11->Location = System::Drawing::Point(38, 255);
 			this->textBox11->Name = L"textBox11";
-			this->textBox11->Size = System::Drawing::Size(100, 22);
+			this->textBox11->Size = System::Drawing::Size(100, 20);
 			this->textBox11->TabIndex = 59;
 			// 
 			// textBox12
 			// 
-			this->textBox12->Location = System::Drawing::Point(50, 247);
+			this->textBox12->Location = System::Drawing::Point(38, 275);
 			this->textBox12->Name = L"textBox12";
-			this->textBox12->Size = System::Drawing::Size(100, 22);
+			this->textBox12->Size = System::Drawing::Size(100, 20);
 			this->textBox12->TabIndex = 60;
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(273, 90);
+			this->button2->Location = System::Drawing::Point(261, 275);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 61;
@@ -387,90 +363,90 @@ namespace Project1 {
 			// label10
 			// 
 			this->label10->AutoSize = true;
-			this->label10->Location = System::Drawing::Point(284, 147);
+			this->label10->Location = System::Drawing::Point(307, 55);
 			this->label10->Name = L"label10";
-			this->label10->Size = System::Drawing::Size(321, 169);
+			this->label10->Size = System::Drawing::Size(298, 156);
 			this->label10->TabIndex = 63;
 			this->label10->Text = resources->GetString(L"label10.Text");
 			// 
 			// label12
 			// 
 			this->label12->AutoSize = true;
-			this->label12->Location = System::Drawing::Point(209, 76);
+			this->label12->Location = System::Drawing::Point(218, 255);
 			this->label12->Name = L"label12";
-			this->label12->Size = System::Drawing::Size(176, 13);
+			this->label12->Size = System::Drawing::Size(166, 13);
 			this->label12->TabIndex = 64;
 			this->label12->Text = L"Фсв (общий световой поток) = ";
 			// 
 			// label13
 			// 
 			this->label13->AutoSize = true;
-			this->label13->Location = System::Drawing::Point(156, 30);
+			this->label13->Location = System::Drawing::Point(144, 58);
 			this->label13->Name = L"label13";
-			this->label13->Size = System::Drawing::Size(23, 13);
+			this->label13->Size = System::Drawing::Size(21, 13);
 			this->label13->TabIndex = 65;
 			this->label13->Text = L"[м]";
 			// 
 			// label14
 			// 
 			this->label14->AutoSize = true;
-			this->label14->Location = System::Drawing::Point(156, 50);
+			this->label14->Location = System::Drawing::Point(144, 78);
 			this->label14->Name = L"label14";
-			this->label14->Size = System::Drawing::Size(23, 13);
+			this->label14->Size = System::Drawing::Size(21, 13);
 			this->label14->TabIndex = 66;
 			this->label14->Text = L"[м]";
 			// 
 			// label16
 			// 
 			this->label16->AutoSize = true;
-			this->label16->Location = System::Drawing::Point(156, 70);
+			this->label16->Location = System::Drawing::Point(144, 98);
 			this->label16->Name = L"label16";
-			this->label16->Size = System::Drawing::Size(23, 13);
+			this->label16->Size = System::Drawing::Size(21, 13);
 			this->label16->TabIndex = 67;
 			this->label16->Text = L"[м]";
 			// 
 			// label17
 			// 
 			this->label17->AutoSize = true;
-			this->label17->Location = System::Drawing::Point(156, 90);
+			this->label17->Location = System::Drawing::Point(144, 118);
 			this->label17->Name = L"label17";
-			this->label17->Size = System::Drawing::Size(27, 13);
+			this->label17->Size = System::Drawing::Size(25, 13);
 			this->label17->TabIndex = 68;
 			this->label17->Text = L"[лк]";
 			// 
 			// label18
 			// 
 			this->label18->AutoSize = true;
-			this->label18->Location = System::Drawing::Point(156, 110);
+			this->label18->Location = System::Drawing::Point(144, 138);
 			this->label18->Name = L"label18";
-			this->label18->Size = System::Drawing::Size(27, 13);
+			this->label18->Size = System::Drawing::Size(25, 13);
 			this->label18->TabIndex = 69;
 			this->label18->Text = L"[лк]";
 			// 
 			// label21
 			// 
 			this->label21->AutoSize = true;
-			this->label21->Location = System::Drawing::Point(156, 130);
+			this->label21->Location = System::Drawing::Point(144, 158);
 			this->label21->Name = L"label21";
-			this->label21->Size = System::Drawing::Size(27, 13);
+			this->label21->Size = System::Drawing::Size(25, 13);
 			this->label21->TabIndex = 70;
 			this->label21->Text = L"[лк]";
 			// 
 			// label22
 			// 
 			this->label22->AutoSize = true;
-			this->label22->Location = System::Drawing::Point(156, 170);
+			this->label22->Location = System::Drawing::Point(144, 198);
 			this->label22->Name = L"label22";
-			this->label22->Size = System::Drawing::Size(22, 13);
+			this->label22->Size = System::Drawing::Size(20, 13);
 			this->label22->TabIndex = 71;
 			this->label22->Text = L"[В]";
 			// 
 			// label24
 			// 
 			this->label24->AutoSize = true;
-			this->label24->Location = System::Drawing::Point(156, 190);
+			this->label24->Location = System::Drawing::Point(144, 218);
 			this->label24->Name = L"label24";
-			this->label24->Size = System::Drawing::Size(27, 13);
+			this->label24->Size = System::Drawing::Size(24, 13);
 			this->label24->TabIndex = 72;
 			this->label24->Text = L"[м²]";
 			// 
@@ -480,7 +456,7 @@ namespace Project1 {
 			this->label1->BackColor = System::Drawing::Color::DarkGray;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(164, 6);
+			this->label1->Location = System::Drawing::Point(143, 9);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(369, 24);
 			this->label1->TabIndex = 62;
@@ -538,10 +514,6 @@ namespace Project1 {
 		}
 #pragma endregion
 
-private: System::Void label19_Click(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void textBox4_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-}
 
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	double L = System::Double::Parse(this->textBox1->Text);
@@ -559,7 +531,10 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	double Fsv = 2 * Fl;
 	int N = static_cast<int>((Eh * Sn * Kz * Z * 100) / (Fsv * eta));
 	double FsvShtrih = (Eh * Sn * Kz * Z * 100) / (N * eta);
+	FsvShtrih = round(FsvShtrih * 100) / 100;
 	label11->Text = FsvShtrih.ToString() + " лк";
+	System::String^ result = "Общий световой поток: " + FsvShtrih.ToString() + " лк" + "\nОбщее количество светильников: " + N;
+	MessageBox::Show(this, result, "Вычисление завершено", MessageBoxButtons::OK);
 }
 
 };
